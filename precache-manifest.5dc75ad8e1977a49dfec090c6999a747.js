e4a493a20527f8a8a677b683df90c399c96867c5
naveenshrinivas <45986597+naveenshrinivas@users.noreply.github.com>
2020-05-27 10:21:17 +0530

    Updates

---
+++ precache-manifest.5dc75ad8e1977a49dfec090c6999a747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39187191a1f62603a683e0955b2ba045",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a559e461848b92f56ac4",
+    "url": "/my-app/static/css/main.f13e3109.chunk.css"
+  },
+  {
+    "revision": "7801bfc80a3313b373ea",
+    "url": "/my-app/static/js/2.2407abc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2407abc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a559e461848b92f56ac4",
+    "url": "/my-app/static/js/main.3911f92a.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
